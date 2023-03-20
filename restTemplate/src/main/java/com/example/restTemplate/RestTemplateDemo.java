a378c89db9d28de8b8e715f69f706f1102f12180
KTakev <kostadin.takev@gmail.com>
2023-03-20 19:15:24 +0200

    connecting two servers via RestTemplate

--- restTemplate/src/main/java/com/example/restTemplate/RestTemplateDemo.java
+++ restTemplate/src/main/java/com/example/restTemplate/RestTemplateDemo.java
@@ -1,2 +1,29 @@
-package com.example.restTemplate;public class RestTemplateDemo {
+package com.example.restTemplate;
+
+import com.example.restTemplate.model.BookDTO;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class RestTemplateDemo implements CommandLineRunner {
+
+    private RestTemplate restTemplate;
+
+    public RestTemplateDemo(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Override
+    public void run(String... args) {
+        ResponseEntity<BookDTO[]> allBooksResponse = restTemplate
+                .getForEntity("http://localhost:8080/api/books", BookDTO[].class);
+
+        if (allBooksResponse.hasBody()) {
+            for (BookDTO book : allBooksResponse.getBody()) {
+                System.out.println("Book: " + book);
+            }
+        }
+    }
 }
