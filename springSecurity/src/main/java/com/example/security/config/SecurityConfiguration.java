018452f7c60257f1c52a247d652aea4299eecd41
KTakev <kostadin.takev@gmail.com>
2023-03-24 14:26:53 +0200

    implementing SecurytiFilterChain Bean

--- springSecurity/src/main/java/com/example/security/config/SecurityConfiguration.java
+++ springSecurity/src/main/java/com/example/security/config/SecurityConfiguration.java
@@ -1,7 +1,9 @@
 package com.example.security.config;
 
+import com.example.security.model.enums.UserRoleEnum;
 import com.example.security.repository.UserRepository;
 import com.example.security.service.AppUserDetailsService;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfiguration {
@@ -23,9 +26,48 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public SecurityFilterChain filterChain (HttpSecurity httpSecurity) {
-        //TODO
-        return null;
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+
+        http
+        //define which requests are allowed and which not:
+                .authorizeRequests()
+                //everyone could download this static resources(which are css, js and images)
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                //everyone could access this pages:
+                .antMatchers("/", "/users/login", "/users/register").permitAll()
+                //only users with role MODERATOR can access this page:
+                .antMatchers("/pages/moderators").hasRole(UserRoleEnum.MODERATOR.name())
+                //only user with ADMIN role can access this page:
+                .antMatchers("/pages/admins").hasRole(UserRoleEnum.ADMIN.name())
+                //only logged-in users can access other pages:
+                .anyRequest().authenticated()
+              //generating the login page:
+              .and()
+                //configuration of the login form:
+                .formLogin()
+                //the custom login form:
+                .loginPage("/users/login")
+                //the name of the username form field (it has to match with the name in the html form!!!):
+                .usernameParameter(UsernamePasswordAuthenticationFilter
+                        .SPRING_SECURITY_FORM_USERNAME_KEY)
+                //the name of the password form field (it has to match with the name in the html form!!!):
+                .usernameParameter(UsernamePasswordAuthenticationFilter
+                        .SPRING_SECURITY_FORM_PASSWORD_KEY)
+                //the page shown if the login is successful (the home page):
+                .defaultSuccessUrl("/")
+                //if the login isn't successful:
+                //todo: controller for "/users/login-error"
+                .failureForwardUrl("/users/login-error")
+              //generating the logout page:
+              .and()
+                //same as for login! but using logout():
+                .logout()
+                .logoutUrl("/users/logout")
+                //invalidating the session and deleting the cookies of the logout user:
+                .invalidateHttpSession(true).deleteCookies("JSESSIONID");
+
+       //build() calls SecurityFilterChain:
+        return http.build();
     }
 
     @Bean
